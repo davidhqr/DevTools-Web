ef68c2de3208bc415c9ddaa740f54d91362b59a1
David Zhang <david.zhang2@shopify.com>
2019-03-31 18:49:13 -0400

    Add LineBreakRemover tool

---
+++ pages/string/line-break-remover.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import LineBreakRemover from "../../components/tools/string/LineBreakRemover.js";
+
+export default class LineBreakRemoverIndex extends Component {
+  render () {
+    return <LineBreakRemover />
+  }
+}
